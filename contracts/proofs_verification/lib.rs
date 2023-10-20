b1575cf33bdd4ea740ca610677e78ee048c7836d
Misha Kushka <kushka.misha@gmail.com>
2023-10-20 16:06:30 -0700

    Full proofs_verification.verify() test

--- contracts/proofs_verification/lib.rs
+++ contracts/proofs_verification/lib.rs
@@ -2,9 +2,7 @@
 
 #[ink::contract]
 mod proofs_verification {
-    use hex::FromHex;
     use ink::prelude::vec::Vec;
-    use ink_env::hash::CryptoHash;
     use tiny_keccak::{Hasher, Keccak};
 
     #[ink(storage)]
@@ -34,6 +32,32 @@ mod proofs_verification {
 
         #[ink(message)]
         pub fn verify(&self, signer: AccountId, data_hash: [u8; 32], signature: [u8; 65]) -> bool {
+            // Convert data hash to Etereum signed message format
+            let msg_hash = Self::to_eth_signed_message_hash(data_hash);
+
+            // Recover ECDSA signature
+            let mut uncompressed_public_key = [0; 33];
+            let _ = ink_env::ecdsa_recover(&signature, &msg_hash, &mut uncompressed_public_key);
+            ink::env::debug_println!("uncompressed_public_key {:?}", uncompressed_public_key);
+
+            // Public key to Eth address
+            let mut account_id_bytes = [0; 20];
+            let _ = ink_env::ecdsa_to_eth_address(&uncompressed_public_key, &mut account_id_bytes);
+
+            // Validate that recovered Eth address matches the expected address
+            if signer
+                == AccountId::from({
+                    let mut tmp = [0u8; 32];
+                    tmp[12..].copy_from_slice(&account_id_bytes);
+                    tmp
+                })
+            {
+                return true;
+            }
+            false
+        }
+
+        fn to_eth_signed_message_hash(data_hash: [u8; 32]) -> [u8; 32] {
             let mut hasher = Keccak::v256();
             let prefix = b"\x19Ethereum Signed Message:\n32"; // Ethereum's message prefix
 
@@ -51,40 +75,14 @@ mod proofs_verification {
 
             ink::env::debug_println!("msg hash bytes {:?}", msg_hash);
 
-            // TODO: import cryptography library for ECDSA
-            let mut uncompressed_public_key = [0; 33];
-            let _ = ink_env::ecdsa_recover(&signature, &msg_hash, &mut uncompressed_public_key);
-
-            ink::env::debug_println!("uncompressed_public_key {:?}", uncompressed_public_key);
-
-            // // Get AccountId from output (compressed public key)
-            // let mut generated_account_id_data = [0u8; 32];
-            // let _ = ink_env::hash::Blake2x256::hash(
-            //     &uncompressed_public_key,
-            //     &mut generated_account_id_data,
-            // );
-            // let generated_account_id =
-            //     AccountId::from(uncompressed_public_key /*generated_account_id_data*/);
-
-            let mut account_id: [u8; 20] = [0; 20];
-            let _ = ink_env::ecdsa_to_eth_address(&uncompressed_public_key, &mut account_id);
-            let mut generated_account_id = [0; 32];
-            // AccountId::from(account_id);
-            generated_account_id[12..].copy_from_slice(&account_id);
-
-            ink::env::debug_println!("account_id {:?}", generated_account_id);
-            ink::env::debug_println!("signer {:?}", signer);
-
-            if signer == AccountId::from(generated_account_id) {
-                return true;
-            }
-            false
+            msg_hash
         }
     }
 
     #[cfg(test)]
     mod tests {
         use super::*;
+        use hex::FromHex;
 
         #[ink::test]
         fn constructor() {
@@ -92,27 +90,47 @@ mod proofs_verification {
         }
 
         #[ink::test]
-        fn verify_signed_proof_works() {
+        fn verify_works() {
             let contract = ProofsVerification::new();
 
-            // Values from Ethereum. Replace these dummy values with real ones.
-            let signer: AccountId = AccountId::from(
+            let signer_1: AccountId = AccountId::from(
                 <[u8; 32]>::from_hex("00".repeat(12) + "3C44CdDdB6a900fa2b585dd299e03d12FA4293BC")
                     .unwrap(),
             );
-
-            let data: [u8; 32] = <[u8; 32]>::from_hex(
+            let signer_2: AccountId = AccountId::from(
+                <[u8; 32]>::from_hex("00".repeat(12) + "90F79bf6EB2c4f870365E785982E1f101E93b906")
+                    .unwrap(),
+            );
+            let message_1: [u8; 32] = <[u8; 32]>::from_hex(
                 "49145797d5b241d1bc807fddde38d675212624d4556f22246bde8be447967f8e",
             )
             .unwrap();
-            ink::env::debug_println!("data hash {:?}", data);
+            let message_2: [u8; 32] = <[u8; 32]>::from_hex(
+                "0413d1a7675fa28b9c504ace913dc39e627894d5775c840a06a245d500ce5009",
+            )
+            .unwrap();
+            let signature_1: [u8; 65] = <[u8; 65]>::from_hex("1fccf4ff11872b0426c7b7826db58b876d9d02d78759b5a7bccc1035f451248b5369ccbebc00094043f55746733f975dfbf7c2000c204853a9ea38ebccf8eb101c").unwrap();
+            let signature_2: [u8; 65] = <[u8; 65]>::from_hex("78ed8734ca4fc72cedfdb2eee4080cc0e2802dc12a7cac5906839a96dd60f0ea4e05fcfab9181fffa5a694a02ca0ba3eee87a842ffc64c66a8fd0ff6780db0c31b").unwrap();
 
-            let signature: [u8; 65] = <[u8; 65]>::from_hex("1fccf4ff11872b0426c7b7826db58b876d9d02d78759b5a7bccc1035f451248b5369ccbebc00094043f55746733f975dfbf7c2000c204853a9ea38ebccf8eb101c").unwrap();
-            ink::env::debug_println!("signature {:?}", signature);
-
-            // Verify that the signature is valid
-            let is_valid = contract.verify(signer, data, signature);
+            // correct signer of message1
+            let is_valid = contract.verify(signer_1, message_1, signature_1);
             assert_eq!(is_valid, true);
+
+            // wrong signer of message1
+            let is_valid = contract.verify(signer_2, message_1, signature_1);
+            assert_eq!(is_valid, false);
+
+            // correct signer of message2
+            let is_valid = contract.verify(signer_2, message_2, signature_2);
+            assert_eq!(is_valid, true);
+
+            // wrong signer of message2
+            let is_valid = contract.verify(signer_1, message_2, signature_2);
+            assert_eq!(is_valid, false);
+
+            // wrong signature of message2
+            let is_valid = contract.verify(signer_1, message_2, signature_1);
+            assert_eq!(is_valid, false);
         }
     }
 }
