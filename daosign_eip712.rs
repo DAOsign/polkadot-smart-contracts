20f33a36daea05e0a243ca775cf2525c80a492d0
Misha Kushka <kushka.misha@gmail.com>
2023-11-30 10:41:11 -0800

    Define all structs as per Solidity impl
    https://github.com/DAOsign/ethereum-smart-contracts/blob/a2f1baa0d7b902dcff450e4947b15406a692594d/contracts/DAOSignEIP712.sol

--- daosign_eip712.rs
+++ daosign_eip712.rs
@@ -1,7 +1,157 @@
 #![cfg_attr(not(feature = "std"), no_std, no_main)]
 
-#[openbrush::contract]
+#[ink::contract]
 mod daosign_eip712 {
+    use ink::prelude::{string::String, vec::Vec};
+    use scale::{Decode, Encode};
+
+    //
+    // structs definitions
+    //
+    #[derive(Debug, Clone, PartialEq, Eq, Decode, Encode)]
+    #[cfg_attr(
+        feature = "std",
+        derive(scale_info::TypeInfo, ink::storage::traits::StorageLayout)
+    )]
+    pub struct EIP712Domain {
+        name: String,
+        version: String,
+        chain_id: u128,
+        verifying_contract: AccountId,
+    }
+
+    #[derive(Debug, Clone, PartialEq, Eq, Decode, Encode)]
+    #[cfg_attr(
+        feature = "std",
+        derive(scale_info::TypeInfo, ink::storage::traits::StorageLayout)
+    )]
+    pub struct Signer {
+        addr: AccountId,
+        metadata: String,
+    }
+
+    #[derive(Debug, Clone, PartialEq, Eq, Decode, Encode)]
+    #[cfg_attr(
+        feature = "std",
+        derive(scale_info::TypeInfo, ink::storage::traits::StorageLayout)
+    )]
+    pub struct ProofOfAuthority {
+        name: String,
+        from: AccountId,
+        agreement_cid: String,
+        signers: Vec<Signer>,
+        app: String,
+        timestamp: u128,
+        metadata: String,
+    }
+
+    #[derive(Debug, Clone, PartialEq, Eq, Decode, Encode)]
+    #[cfg_attr(
+        feature = "std",
+        derive(scale_info::TypeInfo, ink::storage::traits::StorageLayout)
+    )]
+    pub struct ProofOfSignature {
+        name: String,
+        signer: AccountId,
+        agreement_cid: String,
+        app: String,
+        timestamp: u128,
+        metadata: String,
+    }
+
+    #[derive(Debug, Clone, PartialEq, Eq, Decode, Encode)]
+    #[cfg_attr(
+        feature = "std",
+        derive(scale_info::TypeInfo, ink::storage::traits::StorageLayout)
+    )]
+    pub struct ProofOfAgreement {
+        agreement_cid: String,
+        signature_cids: Vec<String>,
+        app: String,
+        timestamp: u128,
+        metadata: String,
+    }
+
+    #[derive(Debug, Clone, PartialEq, Eq, Decode, Encode)]
+    #[cfg_attr(
+        feature = "std",
+        derive(scale_info::TypeInfo, ink::storage::traits::StorageLayout)
+    )]
+    pub struct EIP712PropertyType {
+        name: String,
+        kind: String,
+    }
+
+    #[derive(Debug, Clone, PartialEq, Eq, Decode, Encode)]
+    #[cfg_attr(
+        feature = "std",
+        derive(scale_info::TypeInfo, ink::storage::traits::StorageLayout)
+    )]
+    pub struct EIP712ProofOfAuthorityTypes {
+        eip712_domain: Vec<EIP712PropertyType>,
+        signer: Vec<EIP712PropertyType>,
+        proof_of_authority: Vec<EIP712PropertyType>,
+    }
+
+    #[derive(Debug, Clone, PartialEq, Eq, Decode, Encode)]
+    #[cfg_attr(
+        feature = "std",
+        derive(scale_info::TypeInfo, ink::storage::traits::StorageLayout)
+    )]
+    pub struct EIP712ProofOfAuthority {
+        types: EIP712ProofOfAuthorityTypes,
+        domain: EIP712Domain,
+        primary_type: String,
+        message: ProofOfAuthority,
+    }
+
+    #[derive(Debug, Clone, PartialEq, Eq, Decode, Encode)]
+    #[cfg_attr(
+        feature = "std",
+        derive(scale_info::TypeInfo, ink::storage::traits::StorageLayout)
+    )]
+    pub struct EIP712ProofOfSignatureTypes {
+        eip712_domain: Vec<EIP712PropertyType>,
+        proof_of_signature: Vec<EIP712PropertyType>,
+    }
+
+    #[derive(Debug, Clone, PartialEq, Eq, Decode, Encode)]
+    #[cfg_attr(
+        feature = "std",
+        derive(scale_info::TypeInfo, ink::storage::traits::StorageLayout)
+    )]
+    pub struct EIP712ProofOfSignature {
+        types: EIP712ProofOfSignatureTypes,
+        domain: EIP712Domain,
+        primary_type: String,
+        message: ProofOfSignature,
+    }
+
+    #[derive(Debug, Clone, PartialEq, Eq, Decode, Encode)]
+    #[cfg_attr(
+        feature = "std",
+        derive(scale_info::TypeInfo, ink::storage::traits::StorageLayout)
+    )]
+    pub struct EIP712ProofOfAgreementTypes {
+        eip712_domain: Vec<EIP712PropertyType>,
+        proof_of_agreement: Vec<EIP712PropertyType>,
+    }
+
+    #[derive(Debug, Clone, PartialEq, Eq, Decode, Encode)]
+    #[cfg_attr(
+        feature = "std",
+        derive(scale_info::TypeInfo, ink::storage::traits::StorageLayout)
+    )]
+    pub struct EIP712ProofOfAgreement {
+        types: EIP712ProofOfAgreementTypes,
+        domain: EIP712Domain,
+        primary_type: String,
+        message: ProofOfAgreement,
+    }
+
+    //
+    // DAOsignEIP712 contract
+    //
 
     #[ink(storage)]
     pub struct DAOsignEIP712 {}
@@ -9,7 +159,6 @@ mod daosign_eip712 {
     impl DAOsignEIP712 {
         #[ink(constructor)]
         pub fn new() -> Self {
-            ink::env::debug_println!("created new instance at {}", Self::env().block_number());
             Self {}
         }
     }
