6006d4d3b26ebda4c2a23fc84a57e1f87bb336ff
Misha Kushka <kushka.misha@gmail.com>
2024-01-25 09:20:58 -0800

    Move docs to generated-docs folder

--- gen-docs.sh
+++ gen-docs.sh
@@ -1,4 +1,4 @@
 cargo doc --no-deps
-rm -rf ./docs/generated
+rm -rf ./generated-docs
 echo "<meta http-equiv=\"refresh\" content=\"0; url=build_wheel\">" > target/doc/index.html
-cp -r target/doc ./docs/generated
+cp -r target/doc ./generated-docs
